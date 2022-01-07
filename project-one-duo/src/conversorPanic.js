eb9c7291b9199c08994d760334d359d688b2df83
Davi de Almeida Guimarães <odaviloper@gmail.com>
2022-01-06 23:47:49 -0300

    Now the result shows on screen
    Used JS to show the result on screen, before it was only showing on the console.

--- project-one-duo/src/conversorPanic.js
+++ project-one-duo/src/conversorPanic.js
@@ -1,8 +1,23 @@
+const button = document.querySelector(".button");
 function convertToF() {
-    const celsius = document.querySelector(".inputs").value
-    let fahrenheit = celsius * (9/5) + 32;
-    console.log(fahrenheit)
+  const input = document.querySelector(".inputs").value;
+  celsius = input == "" ? "0" : input;
+  const fahrenheit = celsius * (9/5) + 32;
+  console.log(`${celsius}°C = ${fahrenheit.toFixed(2)}°F`);
+
+  function createHtml() {
+    const resultDiv = document.querySelector(".result-div");
+    const result = document.createElement("h1");
+    result.classList.add("result-text");
+    resultDiv.classList.add("menu-row-margin");
+    resultDiv.insertBefore(result, resultDiv.firstChild);
   }
 
-const button = document.querySelector(".button")
+  if(document.querySelector(".result-text") == null) {
+    createHtml();
+  }
+  document.querySelector(".result-text").innerHTML = `${celsius}°C = ${fahrenheit.toFixed(2)}°F`;
+  document.querySelector("#celsius_input").value = "";
+  }
+
 button.addEventListener("click", convertToF);
